17b1ceb4326bcce0594aacc46948991a686802a3
oliceglad <vycheslavofficial@gmail.com>
2024-04-08 17:05:00 +0400

    added .year

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,5 @@
 export const reverseList = (array) => {
-    const newArray = array.sort((a, b) => a > b)
+    const newArray = array.sort((a, b) => a.year > b.year)
+    console.log(newArray);
     return newArray
 }
