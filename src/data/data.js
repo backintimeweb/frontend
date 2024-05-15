dea02112510bff2a70f2357924ebfe1eac279f72
oliceglad <vycheslavofficial@gmail.com>
2024-05-15 21:53:51 +0400

    deleted data

--- src/data/data.js
+++ src/data/data.js
@@ -1,14 +1,3 @@
-import logo2023 from "../assets/2023.png";
-import logo2022 from "../assets/2022.jpg";
-import logo2021 from "../assets/2021.png";
-import logo2020 from "../assets/2020.png";
-
-export const photos = {
-  2023: logo2023,
-  2022: logo2022,
-  2021: logo2021,
-  2020: logo2020,
-};
 
 export const scrollOptions = {
   duration: 500,
