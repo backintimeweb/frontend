41f84cae36d610baba7a63989bc6f0a0d8479c22
oliceglad <vycheslavofficial@gmail.com>
2024-05-18 08:42:52 +0400

    input search year

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,18 +2,25 @@ import { Card } from "../components/Card/Card";
 import InfoIcon from "@mui/icons-material/Info";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useGetAllPostsQuery } from "../api/posts";
-import { reverseList } from "../utils/helpers";
 import { Loader } from "../components/UI/loader";
+import { useState } from "react";
 
 export const MainPage = ({ children }) => {
   const navigate = useNavigate();
 
   const { data: years, isLoading, isError } = useGetAllPostsQuery();
+  const [searchQuery, setSearchQuery] = useState("");
 
   const onClickHandler = (year) => {
     localStorage.setItem("year", year);
     navigate(`/years/${year}`);
   };
+  
+  const filteredYears = years
+    ? years.filter((year) =>
+        year.year.toString().includes(searchQuery)
+      )
+    : [];
 
   return (
     <div className="main">
@@ -26,16 +33,24 @@ export const MainPage = ({ children }) => {
         </NavLink>
       </div>
 
-      {/* <input type="text" /> */}
+      <input
+        type="number"
+        placeholder="Введите год"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className="main__input"
+      />
+
       <div className="main__list">
         {years ? (
-          reverseList([...years]).map((elem, key) => (
+          filteredYears.length > 0 ?
+           filteredYears.map((elem, key) => (
             <Card
               key={key}
               elem={elem.year}
               onClickHandler={() => onClickHandler(elem.year)}
             />
-          ))
+          )) : <div>Ничего не найдено</div>
         ) : isLoading ? (
           <div className="main__loader">
             <Loader width={"100px"} height="100px" />
--- src/utils/helpers.js
+++
@@ -1,4 +0,0 @@
-export const reverseList = (array) => {
-    const newArray = array.sort((a, b) => a.year > b.year)
-    return newArray
-}
