087f75eb100022898a69a375c48532e4605dd6c3
oliceglad <vycheslavofficial@gmail.com>
2024-05-30 22:13:36 +0400

    added navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import s from './Navbar.module.scss'
+
+export const Navbar = () => {
+    return (
+        <nav >
+            <ul>
+                <li>
+                    
+                </li>
+            </ul>
+        </nav>
+    )
+}
